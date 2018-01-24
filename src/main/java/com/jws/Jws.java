915d5735a3998a09d05d9f7d1c006353056772db
Administrator <Administrator@HOME-PC>
2018-01-24 16:10:26 +0800

    Minor bug fixes.

--- src/main/java/com/jws/Jws.java
+++ src/main/java/com/jws/Jws.java
@@ -1,9 +1,13 @@
 package com.jws;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.Properties;
 
+import javax.annotation.Resource;
 import javax.jws.WebService;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,11 +15,31 @@ import org.apache.logging.log4j.Logger;
 import com.alibaba.fastjson.JSON;
 import com.data.Oracle2Redis;
 import com.data.Redis2WebService;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.xml.internal.ws.developer.JAXWSProperties;
 
 @WebService
 public class Jws {
 	// 获得Logger
 	private static final Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+
+	@Resource
+	private WebServiceContext wsContext;
+
+	/**
+	 * 输出InetSocketAddress、HostAddress和HostName
+	 */
+	private void get() {
+		try {
+			MessageContext mc = wsContext.getMessageContext();
+			HttpExchange exchange = (HttpExchange) mc.get(JAXWSProperties.HTTP_EXCHANGE);
+			InetSocketAddress isa = exchange.getRemoteAddress();
+			logger.info("InetSocketAddress : " + isa);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.warn(e);
+		}
+	}
 
 	/**
 	 * 用户名密码校验
@@ -58,6 +82,8 @@ public class Jws {
 	 */
 	public boolean manualExtractData(String password, int index) {
 
+		this.get();
+
 		String user = "zhouqingbiao";
 		logger.info("user:" + user);
 		logger.info("password:" + password);
@@ -83,6 +109,7 @@ public class Jws {
 	 * @return
 	 */
 	public String getData(String keys, int index, int rows) {
+		this.get();
 		return JSON.toJSONString(new Redis2WebService().getData(keys, index, rows));
 	}
 }
