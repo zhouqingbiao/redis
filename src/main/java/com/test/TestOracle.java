eb0e9698c96651fbf27cec95c93e25471aa921b9
Administrator <Administrator@zqb-PC>
2018-02-09 15:28:24 +0800

    新增Test代码。

---
+++ src/main/java/com/test/TestOracle.java
@@ -0,0 +1,94 @@
+package com.test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.logger.Logger;
+import com.oracle.Oracle;
+
+public class TestOracle {
+
+	public static void main(String[] args) {
+		// OracleLike开始时间
+		long startTime4OracleOfLike = System.nanoTime();
+
+		String fwzl_like = "濮家 13";
+
+		new TestOracle().testOracle(fwzl_like);
+
+		// OracleLike停止时间
+		long stopTime4OracleOfLike = System.nanoTime();
+
+		Logger.logger.info("查询关键字：" + fwzl_like);
+
+		Logger.logger.info("开始时间（OracleLike）：" + startTime4OracleOfLike);
+		Logger.logger.info("停止时间（OracleLike）：" + stopTime4OracleOfLike);
+
+		// Oracle总时间消耗
+		Logger.logger.info("运行时间（OracleLike）：" + (stopTime4OracleOfLike - startTime4OracleOfLike) + "纳秒");
+		Logger.logger.info("运行时间（OracleLike）：" + (stopTime4OracleOfLike - startTime4OracleOfLike) / 1000000 + "毫秒");
+		Logger.logger.info("运行时间（OracleLike）：" + (stopTime4OracleOfLike - startTime4OracleOfLike) / 1000000000 + "秒");
+	}
+
+	public List<Map<String, String>> testOracle(String fwzl_like) {
+
+		// 定义Oralce并获取连接
+		Oracle jracle = new Oracle();
+		Connection connection = jracle.getConnection();
+
+		// 获取fwzl信息
+		StringBuffer sql = new StringBuffer(
+				"SELECT T.FWCODE, T.ID, T.FWZL FROM HZ_GIS.TPS_FW T WHERE T.LSBZ = 0 AND T.FWZL IS NOT NULL AND T.FWSMZQ = 1201");
+
+		// 去除头尾空格
+		fwzl_like = fwzl_like.trim();
+
+		// 如果有空格则执行分割组装
+		if (fwzl_like.indexOf(" ") != -1) {
+			String[] string_fwzl = fwzl_like.split(" ");
+			for (int i = 0; i < string_fwzl.length; i++) {
+				sql.append(" AND T.FWZL LIKE ?");
+			}
+		}
+
+		PreparedStatement preparedStatement = jracle.getPreparedStatement(connection, sql.toString());
+
+		// SQL赋值
+		if (fwzl_like.indexOf(" ") != -1) {
+			String[] string_fwzl = fwzl_like.split(" ");
+			for (int i = 0; i < string_fwzl.length; i++) {
+				try {
+					preparedStatement.setString(i + 1, "%" + string_fwzl[i] + "%");
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		ResultSet resultSet = jracle.getResultSet(preparedStatement);
+
+		// 数据存入Map
+		Map<String, String> map = null;
+		List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+		try {
+			while (resultSet.next()) {
+				map = new HashMap<String, String>();
+				map.put(resultSet.getMetaData().getColumnName(1), resultSet.getString(1));
+				map.put(resultSet.getMetaData().getColumnName(2), resultSet.getString(2));
+				map.put(resultSet.getMetaData().getColumnName(3), resultSet.getString(3));
+				list.add(map);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			jracle.close(resultSet, preparedStatement, connection);
+		}
+		return list;
+	}
+}
---
+++ src/main/java/com/test/TestRedis.java
@@ -0,0 +1,107 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import com.alibaba.fastjson.JSON;
+import com.logger.Logger;
+import com.redis.Redis;
+
+import redis.clients.jedis.Jedis;
+
+public class TestRedis {
+
+	public static void main(String[] args) {
+		// Redis开始时间
+		long startTime4Redis = System.nanoTime();
+
+		String keys = "*濮家*13*";
+
+		int index = 0;
+
+		int rows = 1000;
+
+		new TestRedis().testRedis(keys, index, rows);
+
+		// Redis停止时间
+		long stopTime4Redis = System.nanoTime();
+
+		Logger.logger.info("查询关键字：" + keys);
+
+		Logger.logger.info("开始时间（Redis）：" + startTime4Redis);
+		Logger.logger.info("停止时间（Redis）：" + stopTime4Redis);
+
+		// Redis总时间消耗
+		Logger.logger.info("运行时间（Redis）：" + (stopTime4Redis - startTime4Redis) + "纳秒");
+		Logger.logger.info("运行时间（Redis）：" + (stopTime4Redis - startTime4Redis) / 1000000 + "毫秒");
+		Logger.logger.info("运行时间（Redis）：" + (stopTime4Redis - startTime4Redis) / 1000000000 + "秒");
+	}
+
+	public ArrayList<Object> testRedis(String keys, int index, int rows) {
+
+		// 过滤垃圾信息
+
+		Logger.logger.info("keys=" + keys);
+		Logger.logger.info("index=" + index);
+		Logger.logger.info("rows=" + rows);
+
+		// keys不能为空。
+		if (null == keys || "".equals(keys)) {
+			Logger.logger.info("keys不能为空。");
+			return null;
+		}
+
+		// 小于0返回null
+		if (index < 0) {
+			Logger.logger.info("index不能小于零。");
+			return null;
+		}
+
+		// 小于或等于0返回null
+		if (rows <= 0) {
+			Logger.logger.info("rows不能小于或等于零。");
+			return null;
+		}
+
+		// 获得Jedis
+		try (Jedis jedis = Redis.Pool.getResource()) {
+
+			// 判断是否连接成功
+			if (("PONG").equals(jedis.ping())) {
+				Logger.logger.info("Redis连接成功！");
+			} else {
+				Logger.logger.info("Redis连接失败！");
+				return null;
+			}
+
+			// 选择数据库
+			jedis.select(index);
+			Logger.logger.info("选择" + index + "号数据库。");
+
+			// 查询数据库
+			Set<String> set = jedis.keys(keys);
+
+			// 无数据量返回null
+			if (set.isEmpty()) {
+				Logger.logger.info("查询结果为空！");
+				return null;
+			}
+
+			// 定义结果List
+			ArrayList<Object> resultList = new ArrayList<Object>();
+
+			for (String key : set) {
+				// 获取RedisList
+				for (String result : jedis.lrange(key, 0, jedis.llen(key) - 1)) {
+					resultList.add(JSON.parse(result));
+					// 如果条数达到rows则返回List
+					if (resultList.size() == rows) {
+						return resultList;
+					}
+				}
+			}
+			return resultList;
+		}
+
+	}
+}
